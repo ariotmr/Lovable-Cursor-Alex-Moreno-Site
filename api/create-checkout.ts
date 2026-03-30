895aace1a60bfbc9041005537713597fbbf425f8
Amir <arioteimuri@gmail.com>
2026-03-30 18:06:15 -0400

    fix: Stripe API version mismatch in types

--- api/create-checkout.ts
+++ api/create-checkout.ts
@@ -1,9 +1,7 @@
 import { VercelRequest, VercelResponse } from '@vercel/node';
 import Stripe from 'stripe';
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
-  apiVersion: '2023-10-16', // This is standard, but you can upgrade to latest if needed
-});
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   // Only allow POST requests for creating a checkout session
